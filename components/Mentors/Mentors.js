740f5a9491ece14b2d89ac7fb553fbe149518b9a
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-05-20 13:05:04 +0600

    'cards'

---
+++ components/Mentors/Mentors.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import DropdownSelect from './DropdownSelect'
+import Divider from '@mui/material/Divider'
+import MentorCard from '../Home/DiscoverTopMentors/MentorCard'
+import { mentors } from '../../data/mentors'
+import {Grid} from '@mui/material'
+
+const Mentors = () => {
+  return (
+    <div className='min-h-screen bg-white'>
+        <div className='pt-[8rem] px-[8rem]'>
+            <div className='flex'>
+                <DropdownSelect cat = 'Category'/>
+                <DropdownSelect cat = 'Expertise'/>
+                <DropdownSelect cat = 'Sort by'/>
+                <DropdownSelect cat = 'Rating'/>
+            </div>
+            <Divider></Divider>
+            <div className='pt-10'>
+                <Grid container spacing={3}>
+                    {mentors.map((mentor) => (
+                        <>
+                        <Grid item md = {3}>
+                            <MentorCard key={mentor.id} mentor={mentor} />
+                        </Grid>
+                        </>
+                    ))}
+                </Grid>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Mentors
---
+++ pages/mentors.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Mentors from '../components/Mentors/Mentors'
+import Layout from '../components/Layout'
+
+const Menter = () => {
+  return (
+    <Layout>
+        <Mentors/>
+    </Layout>
+  )
+}
+
+export default Menter
