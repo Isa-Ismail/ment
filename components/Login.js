7fb29d246e2b78a48fc177044f3d5352aa034a91
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-05-26 09:54:37 +0600

    'begin'

--- components/Login.js
+++ components/Login.js
@@ -4,7 +4,7 @@ import { Button, TextField } from "@mui/material"
 const Login = () => {
   return (
     <div className='min-h-screen'>
-        <div className='pt-[8rem] px-[8rem] flex justify-center items-center'>
+        <div className='pt-[8rem] md:px-[8rem] flex justify-center items-center'>
             <div className='bg-white w-[25rem] min-h-[30rem] space-y-5 border-[.1rem] rounded-md border-cyan-500 px-12 py-10'>
                 <h1 className='text-xl text-green-600'>Sign Up</h1>
                 <TextField
