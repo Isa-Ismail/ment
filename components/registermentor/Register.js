caa83c77f2eb79d8ab54af22ed2a6fc1f62546aa
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-05-28 19:30:14 +0600

    'data'

--- components/registermentor/Register.js
+++ components/registermentor/Register.js
@@ -2,19 +2,28 @@ import {useState} from 'react'
 
 const Register = () => {
   const [steps, setSteps] = useState(1)
+  
+  const [exp, setExperience] = useState({
+    company: '',
+    title:'',
+    start:'',
+    end:'',
+  })
+  
+  const [education, setEducation] = useState ({ 
+    school: '',
+    degree: '',
+    start: '',
+    end: ''
+  })
+
   const [form , setForm] = useState ({
     category: '',
     expertise:'',
     level:'',
     credits: '',
-    company:'',
-    title:'',
-    companyStartDate:'',
-    companyEndDate: '',
-    school:'',
-    degree:'',
-    educationStartDate: '',
-    educationEndDate: '',
+    experience: [],
+    education: [],
     story: '',
     bio:'',
     linkedIn:'',
@@ -22,10 +31,18 @@ const Register = () => {
     website: ''
   })
 
+
   console.log(form)
 
   const handChange = (e) => {
     setForm({...form, [e.target.name]:e.target.value})
+  }
+
+  const handExp = (e) => {
+    setExperience({...exp, [e.target.name]:e.target.value})
+  }
+  const handEdu = (e) => {
+    setEducation({...education, [e.target.name]:e.target.value})
   }
 
   const plusStep = (e) => {
@@ -37,20 +54,27 @@ const Register = () => {
   }
 
   return (
+
+    //Left side of the steps
     <div className={wrapper}>
         <div className={container}>
           <div className="flex flex-col md:space-y-4">
-            <h1 className="font-bold border-[1px] border-cyan-600 p-1">Session Details</h1>
+            <h1 className="font-bold border-[1px] border-cyan-600 p-1 rounded-md">Session Details</h1>
             <h1>Experience</h1>
             <h1>Education</h1>
             <h1>About</h1>
             <h1>Photos</h1>
             <h1>General availability</h1>
           </div>
+          {/* End of left side */}
+
+          {/* Step 1 of step 6 */}
           {steps===1&&(
           <div className="flex flex-col md:pl-10 space-y-4">
             <a className='font-extralight'>step {steps} of 6</a>
             <h1>Select topics you would like to mentor on.</h1><br />
+
+            {/* category */}
             <div className="flex flex-col space-y-2">
               <h1>Tell us about your top mentorship categories *</h1>
               <select name='category' onChange={handChange} value={form.category} className='rounded-md border-[1px] cursor-pointer px-[1rem] py-[.5rem] border-cyan-700' id="standard-select">
@@ -60,7 +84,9 @@ const Register = () => {
                 <option value="Engineering">Engineering</option>
               </select>
             </div>
+            {/* category */}
 
+            {/* expertise */}
             <div className="flex flex-col space-y-4">
               <h1>Add your expertise within the topic. *</h1>
               <select name='expertise' onChange={handChange} value={form.expertise} className='rounded-md border-[1px] cursor-pointer px-[1rem] py-[.5rem] border-cyan-700' id="standard-select">
@@ -70,7 +96,9 @@ const Register = () => {
                 <option value="Full stack">Full stack</option>
               </select>
             </div>
+            {/* expertise */}
 
+            {/* level of expertise */}
             <div className="flex flex-col space-y-4">
               <h1>What is your level of expertise? *</h1>
               <select name='level' onChange={handChange} value={form.level} className='rounded-md border-[1px] cursor-pointer px-[1rem] py-[.5rem] border-cyan-700' id="standard-select">
@@ -80,7 +108,9 @@ const Register = () => {
                 <option value="Expert">Expert</option>
               </select>
             </div>
+            {/* level of expertise */}
 
+            {/* Credits */}
             <div className="flex flex-col space-y-4">
               <h1>Select credits *</h1>
               <select name='credits' onChange={handChange} value={form.credits} className='rounded-md border-[1px] cursor-pointer px-[1rem] py-[.5rem] border-cyan-700' id="standard-select">
@@ -90,6 +120,7 @@ const Register = () => {
                 <option value="Option 3">option 3</option>
               </select>
             </div>
+            {/* Credits */}
 
             <br />
             <div className="flex space-x-[20rem] justify-end">
@@ -103,39 +134,75 @@ const Register = () => {
           <div className="flex flex-col md:pl-10 space-y-4">
           <a className='font-extralight'>step {steps} of 6</a>
             <h1>Love it, tell us more about what you do.</h1><br />
+
             <div className="flex flex-col space-y-2">
+              {/* company */}
               <h1>Company *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
               name='company'
-              value={form.company}
-              onChange={handChange}
+              value={exp.company}
+              onChange={handExp}
               />
+              {/* company */}
+
+              {/* title */}
               <h1>Your title *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
               name='title'
-              value={form.title}
-              onChange={handChange}
+              value={exp.title}
+              onChange={handExp}
               />
+              {/* title */}
+
+              {/* start */}
               <h1>Start date *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
-              name='companyStartDate'
-              value={form.companyStartDate}
-              onChange={handChange}
+              name='start'
+              value={exp.start}
+              onChange={handExp}
               />
+              {/* start */}
 
+              {/* end */}
               <h1>End date *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
-              name='companyEndDate'
-              value={form.companyEndDate}
-              onChange={handChange}
+              name='end'
+              value={exp.end}
+              onChange={handExp}
               />
-              
+              {/* end */}
+
+
+              <button 
+                disabled={!(exp.company&&exp.title&&exp.start&&exp.end)}
+                onClick={()=>{
+                setForm({...form, experience:[...form.experience, exp]})
+                setExperience({company: '',
+                title:'',
+                start:'',
+                end:'',})}}>
+                + Add another experience
+              </button>
             </div>
           
             <br />
             <div className="flex space-x-[20rem]">
             <h1 className='cursor-pointer' onClick = {backStep}> {'<'} Back</h1>
-            <button disabled={!(form.company&&form.title&&form.companyStartDate&&form.companyEndDate)} className='bg-mentmeBlue px-4 py-1 rounded-md text-white' onClick = {plusStep}>Next</button>
+            <button disabled={!(exp.company&&exp.title&&exp.start&&exp.end)} className='bg-mentmeBlue px-4 py-1 rounded-md text-white'
+             
+             onClick = {() =>{ 
+               
+                setForm({...form, experience:[...form.experience, exp]})
+                setExperience({company: '',
+                title:'',
+                start:'',
+                end:''})
+                setSteps(steps+1)
+              }}
+                >
+                Next
+                </button>
+            
             </div>
           </div>
           )}
@@ -143,40 +210,75 @@ const Register = () => {
           {steps===3&&(
           <div className="flex flex-col md:pl-10 space-y-4">
           <a className='font-extralight'>step {steps} of 6</a>
-            <h1>We would love to know about your education.</h1><br />
+            <h1>Love it, tell us more about what you do.</h1><br />
             <div className="flex flex-col space-y-2">
-              <h1>school *</h1>
+
+              {/* school */}
+              <h1>School/Platform *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
               name='school'
-              value={form.school}
-              onChange={handChange}
+              value={education.school}
+              onChange={handEdu}
               />
-              <h1>Your Degree *</h1>
+              {/* school */}
+
+              {/* degree */}
+              <h1>Your degree *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
               name='degree'
-              value={form.degree}
-              onChange={handChange}
+              value={education.degree}
+              onChange={handEdu}
               />
+              {/* degree */}
+
+              {/* start */}
               <h1>Start date *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
-              name='educationStartDate'
-              value={form.educationStartDate}
-              onChange={handChange}
+              name='start'
+              value={education.start}
+              onChange={handEdu}
               />
+              {/* start */}
 
+              {/* end */}
               <h1>End date *</h1>
               <input className='border-[1px] border-mentmeBlue px-3 py-3 rounded-md' 
-              name='educationEndDate'
-              value={form.educationEndtDate}
-              onChange={handChange}
+              name='end'
+              value={education.end}
+              onChange={handEdu}
               />
+              {/* end */}
               
+              <button
+                disabled={!(education.school&&education.degree&&education.start&&education.end)}
+                onClick={()=>{
+                setForm({...form, education:[...form.education, education]})
+                setEducation({school: '',
+                degree:'',
+                start:'',
+                end:'',})}}>
+                + Add another education
+              </button>
             </div>
           
             <br />
             <div className="flex space-x-[20rem]">
-            <h1 className='cursor-pointer' onClick = {backStep}>{'<'} Back</h1>
-            <button disabled={!(form.school&&form.degree&&form.educationStartDate&&form.educationEndDate)} className='bg-mentmeBlue px-4 py-1 rounded-md text-white' onClick = {plusStep}>Next</button>
+            <h1 className='cursor-pointer' onClick = {backStep}> {'<'} Back</h1>
+            <button disabled={!(education.school&&education.degree&&education.start&&education.end)} className='bg-mentmeBlue px-4 py-1 rounded-md text-white'
+             
+             onClick = {() =>{ 
+               
+                setForm({...form, education:[...form.education, education]})
+                setEducation({school: '',
+                degree:'',
+                start:'',
+                end:''})
+                setSteps(steps+1)
+              }}
+                >
+                Next
+                </button>
+            
             </div>
           </div>
           )}
